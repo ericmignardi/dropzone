0a37ca19ec4b89b74c6027f6cf7573290d8ea157
Eric Mignardi <mignardi.e@gmail.com>
2026-02-10 05:40:52 -0500

    Added production frontend URL to cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -61,7 +61,12 @@ const swaggerOptions = {
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
